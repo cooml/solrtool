68423a177c0358b450a6218f879c2ea160511572
luoyongsheng <luoys4@lenovo.com>
2020-09-02 16:53:46 +0800

    update BinarySerialize

---
+++ BinarySerialize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solrtool
+{
+  class BinarySerialize
+  {
+    public static void Serialize<T>(T t, string strFile)
+    {
+
+      using (FileStream fs = new FileStream(strFile, FileMode.Create))
+      {
+
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        formatter.Serialize(fs, t);
+
+      }
+
+    }
+
+    public static T DeSerialize<T>(string strFile)
+    {
+
+      T t = default(T);
+      if (File.Exists(strFile))
+      {
+        using (FileStream fs = new FileStream(strFile, FileMode.Open))
+        {
+          BinaryFormatter formatter = new BinaryFormatter();
+          t = (T)formatter.Deserialize(fs);
+        }
+      }
+
+
+      return t;
+
+    }
+  }
+}
---
+++ suggestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solrtool
+{
+  [Serializable]
+  public class suggestModel
+  {
+    public string appId { get; set; }
+
+    public string scenario { get; set; }
+    public string language { get; set; }
+    public string realm { get; set; }
+    public string type { get; set; }
+    public string word { get; set; }
+
+
+  }
+}
