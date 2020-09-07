977f5deb465f76f983c5dc345458cba8efdef9b4
luoyongsheng <luoys4@lenovo.com>
2020-09-07 20:08:19 +0800

    update suggestList

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace solrtool
 {
@@ -9,6 +12,58 @@ namespace solrtool
     {
         static void Main(string[] args)
         {
+            //var solrStrBinary = BinarySerialize.DeSerialize<List<string>>("solrResponseStrBinary_2020-08-15-15-38-45");
+            var solrBinary = BinarySerialize.DeSerialize<List<SolrResponse>>("solrResponseBinary_2020-08-15-15-38-15");
+            var openSearchSuggest = BinarySerialize.DeSerialize<List<suggestModel>>("opensearchSuggest_2020-09-02-16-10-15");
+            List<suggestModel> suggestList = new List<suggestModel>();
+            foreach (var item in solrBinary)
+            {
+                if (item != null && item.response != null && item.response.docs != null)
+                {
+                    foreach (var itemResponse in item.response.docs)
+                    {
+                        if (itemResponse.query_s != null && itemResponse.query_s.Length < 60 && !itemResponse.query_s.Contains("�"))
+                        {
+                            var r = "";
+                            var l = "";
+                            if (itemResponse.filters_ss != null)
+                            {
+                                foreach (var itemFilter in itemResponse.filters_ss)
+                                {
+                                    if (itemFilter.ToLower().Contains("realm:"))
+                                    {
+                                        r = itemFilter.ToLower().Split(':')[1].Trim();
+                                    }
+                                    if (itemFilter.ToLower().Contains("language:"))
+                                    {
+                                        l = itemFilter.ToLower().Split(':')[1].Trim();
+                                    }
+
+                                }
+                            }
+                            suggestList.Add(new suggestModel
+                            {
+                                appId = "164",
+                                scenario = "2ac2d0fe31384dc8b2ec5aa96ab1a967",
+                                language = l.ToLower(),
+                                realm = r.ToLower(),
+                                type = "keyword",
+                                word = itemResponse.query_s.ToLower()
+
+                            });
+
+                        }
+
+                    }
+                }
+
+            }
+
+            System.IO.File.WriteAllText(@"json.txt", JsonConvert.SerializeObject(suggestList, Newtonsoft.Json.Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }));
+
+            // BinarySerialize.Serialize<List<suggestModel>>(suggestList, "opensearchSuggest_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
+
+
             int start = 0;
             int rows = Convert.ToInt32(ConfigurationManager.AppSettings.Get("rows"));
             int numFound = 0;
@@ -33,13 +88,16 @@ namespace solrtool
                 //  var sDatas = JsonConvert.SerializeObject(resopnseModel.response.docs);
                 //  var sResopnse = SolrHelper.SolrPost(sDatas, toUrl);
                 //}
-
+                List<SolrResponse> solrResponseBinary = new List<SolrResponse>();
+                List<string> solrResponseStrBinary = new List<string>();
                 for (int i = 0; i < (numFound / rows) + 1; i++)
                 {
                     LogHelper.LogInfo("start--------page:" + (i + 1));
 
                     var jsonDataTemp = SolrHelper.getDataFromSolr(fromUrl, rows * i, rows, fq, sort);
+                    solrResponseStrBinary.Add(jsonDataTemp);
                     var resopnseModelTemp = JsonConvert.DeserializeObject<SolrResponse>(jsonDataTemp);
+                    solrResponseBinary.Add(resopnseModelTemp);
                     if (resopnseModelTemp != null && resopnseModelTemp.response != null && resopnseModelTemp.response.docs != null)
                     {
                         if (resopnseModelTemp.response.docs.Count > 0)
@@ -67,6 +125,10 @@ namespace solrtool
 
 
                 }
+                BinarySerialize.Serialize<List<string>>(solrResponseStrBinary, "solrResponseStrBinary_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
+                BinarySerialize.Serialize<List<SolrResponse>>(solrResponseBinary, "solrResponseBinary_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
+
+
             }
             SolrHelper.FusionSolrCommit(toUrl);
 
@@ -76,137 +138,6 @@ namespace solrtool
             Console.WriteLine("press any key/enter to exit");
             Console.Read();
         }
-    
-    
-     static void MainTest(string[] args)
-    {
-      //var solrStrBinary = BinarySerialize.DeSerialize<List<string>>("solrResponseStrBinary_2020-08-15-15-38-45");
-      var solrBinary = BinarySerialize.DeSerialize<List<SolrResponse>>("solrResponseBinary_2020-08-15-15-38-15");
-      var openSearchSuggest= BinarySerialize.DeSerialize<List<suggestModel>>("opensearchSuggest_2020-09-02-16-10-15");
-      List<suggestModel> suggestList = new List<suggestModel>();
-      foreach (var item in solrBinary)
-      {
-        if (item != null && item.response != null && item.response.docs != null)
-        {
-          foreach (var itemResponse in item.response.docs)
-          {
-            if (itemResponse.query_s != null && itemResponse.query_s.Length < 60 && !itemResponse.query_s.Contains("�"))
-            {
-              var r = "";
-              var l = "";
-              if (itemResponse.filters_ss != null)
-              {
-                foreach (var itemFilter in itemResponse.filters_ss)
-                {
-                  if (itemFilter.ToLower().Contains("realm:"))
-                  {
-                    r = itemFilter.ToLower().Split(':')[1].Trim();
-                  }
-                  if (itemFilter.ToLower().Contains("language:"))
-                  {
-                    l = itemFilter.ToLower().Split(':')[1].Trim();
-                  }
 
-                }
-              }
-              suggestList.Add(new suggestModel
-              {
-                appId = "164",
-                scenario = "2ac2d0fe31384dc8b2ec5aa96ab1a967",
-                language = l.ToLower(),
-                realm = r.ToUpper(),
-                type = "keyword",
-                word = itemResponse.query_s.ToLower()
-
-              });
-
-            }
-
-          }
-        }
-
-      }
-
-
-
-     // BinarySerialize.Serialize<List<suggestModel>>(suggestList, "opensearchSuggest_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
-
-
-      int start = 0;
-      int rows = Convert.ToInt32(ConfigurationManager.AppSettings.Get("rows"));
-      int numFound = 0;
-      var fromUrl = ConfigurationManager.AppSettings.Get("fromUrl");
-      var toUrl = ConfigurationManager.AppSettings.Get("toUrl");
-      var sort = ConfigurationManager.AppSettings.Get("sort");
-      var fq = ConfigurationManager.AppSettings.Get("fq");
-      LogHelper.LogInfo("start");
-      LogHelper.LogInfo("from url:" + fromUrl);
-      LogHelper.LogInfo("to url:+" + toUrl);
-      var jsonData = SolrHelper.getDataFromSolr(fromUrl, start, rows, fq, sort);
-      var resopnseModel = JsonConvert.DeserializeObject<SolrResponse>(jsonData);
-      if (resopnseModel != null && resopnseModel.response != null && resopnseModel.response.numFound > 0)
-      {
-        numFound = resopnseModel.response.numFound;
-        LogHelper.LogInfo("numFound:" + numFound);
-
-
-        //if (resopnseModel.response.docs != null && resopnseModel.response.docs.Count > 0)
-        //{
-        //  LogHelper.LogInfo("Import--------:1");
-        //  var sDatas = JsonConvert.SerializeObject(resopnseModel.response.docs);
-        //  var sResopnse = SolrHelper.SolrPost(sDatas, toUrl);
-        //}
-        List<SolrResponse> solrResponseBinary = new List<SolrResponse>();
-        List<string> solrResponseStrBinary = new List<string>();
-        for (int i = 0; i < (numFound / rows) + 1; i++)
-        {
-          LogHelper.LogInfo("start--------page:" + (i + 1));
-
-          var jsonDataTemp = SolrHelper.getDataFromSolr(fromUrl, rows * i, rows, fq, sort);
-          solrResponseStrBinary.Add(jsonDataTemp);
-          var resopnseModelTemp = JsonConvert.DeserializeObject<SolrResponse>(jsonDataTemp);
-          solrResponseBinary.Add(resopnseModelTemp);
-          if (resopnseModelTemp != null && resopnseModelTemp.response != null && resopnseModelTemp.response.docs != null)
-          {
-            if (resopnseModelTemp.response.docs.Count > 0)
-            {
-              //
-              LogHelper.LogInfo("Import--------total:" + ((i + 1) * rows > resopnseModel.response.numFound ? resopnseModel.response.numFound : (i + 1) * rows) + "/" + resopnseModel.response.numFound);
-
-              var sDatas = JsonConvert.SerializeObject(resopnseModelTemp.response.docs);
-              //LogHelper.LogInfo(sDatas);
-
-              var sResopnse = SolrHelper.SolrPost(sDatas, toUrl);
-
-              SolrHelper.FusionSolrCommit(toUrl);
-            }
-            if (resopnseModelTemp.response.docs.Count != rows)
-            {
-              break;//终止
-            }
-          }
-          else
-          {
-            break;
-          }
-
-
-
-        }
-        BinarySerialize.Serialize<List<string>>(solrResponseStrBinary, "solrResponseStrBinary_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
-        BinarySerialize.Serialize<List<SolrResponse>>(solrResponseBinary, "solrResponseBinary_" + System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
-
-
-      }
-      SolrHelper.FusionSolrCommit(toUrl);
-
-
-      LogHelper.LogInfo("complete...");
-
-      Console.WriteLine("press any key/enter to exit");
-      Console.Read();
-    }
-
- 
     }
 }
