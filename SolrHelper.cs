e8df79d5efa5e5725e93abda9393dc8cae16593d
luoyongsheng <luoys4@lenovo.com>
2020-11-18 13:58:31 +0800

    update

--- SolrHelper.cs
+++ SolrHelper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using eService.Microservices.Types.Search;
+using Grpc.Core;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -48,6 +51,7 @@ namespace solrtool
       {
         var queryUrl=baseUrl + "/select?q=" + q + fqString + sortString + rowsString+ startString;
         LogHelper.LogInfo(queryUrl);
+        return getFromPrd(queryUrl);
         oRequest = (HttpWebRequest)WebRequest.Create(queryUrl);
         oRequest.Method = "GET";
         oRequest.ContentType = "application/json;charset=utf-8";
@@ -90,6 +94,34 @@ namespace solrtool
 
 
     }
+
+    public static string getFromPrd(string url)
+    {
+
+      httpRequestClient h = new httpRequestClient();
+      h.baseUrl = url;
+      h.getEncoding = "utf-8";
+      h.contentType = "application/json;charset=utf-8";
+      h.method = "GET";
+
+      Channel channel = new Channel("", ChannelCredentials.Insecure);
+      channel.ConnectAsync(DateTime.UtcNow.AddSeconds(2)).GetAwaiter().GetResult();
+      var client = new SearchGrpc.SearchGrpcClient(channel);
+      var req = new ContentMesReq();
+      req.ReqMes = "";
+      req.ValueMes = JsonConvert.SerializeObject(h, Newtonsoft.Json.Formatting.Indented);
+      string ret = client.SearchExtension(req).ValueMes;
+      return ret;
+
+
+
+    }
+
+
+
+
+
+
     public static string SolrPost(string jsonData, string baseUrl)
     {
       string ret = "";
@@ -144,4 +176,28 @@ namespace solrtool
       return ret;
     }
   }
+
+  public class openSearchIndexModel
+  {
+    public string jsonData { get; set; }
+    public string baseUrl { get; set; }
+    public string appId { get; set; }
+    public string token { get; set; }
+    public string indexType { get; set; }
+    public Dictionary<string, string> openSearchHeaders { get; set; }
+
+
+  }
+  public class httpRequestClient
+  {
+    public string baseUrl { get; set; }
+    public string method { get; set; } = "POST";
+    public byte[] b { get; set; } = new byte[0];
+    public Dictionary<string, string> headers { get; set; }
+    public string contentType { get; set; } = "application/json";
+    public string getEncoding { get; set; } = "utf-8";
+
+  }
+
+
 }
